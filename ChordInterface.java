84254c4b584372ee1e5676b6b2a7ac477cd0b6fc
kandu009 <kandu009@umn.edu>
2015-04-27 10:49:23 -0500

    adding throws RemoteException

--- ChordInterface.java
+++ ChordInterface.java
@@ -1,5 +1,6 @@
 import java.math.BigInteger;
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.util.Hashtable;
 
 /**
@@ -7,26 +8,26 @@ import java.util.Hashtable;
  */
 public interface ChordInterface extends Remote{
 
-    public Node.NodeInfo getMyInfo();
+    public Node.NodeInfo getMyInfo() throws RemoteException;
    
-    public JoinResponse join(String url);
-    public void join_done(Node.NodeInfo newNode);
-    public FindNodeResponsePair find_node(String key, boolean withTrace);
+    public JoinResponse join(String url) throws RemoteException;
+    public void join_done(Node.NodeInfo newNode) throws RemoteException;
+    public FindNodeResponsePair find_node(String key, boolean withTrace) throws RemoteException;
     
-    public String lookup(String word);
-    public Node.NodeInfo predecessor(BigInteger id);
-    public Node.NodeInfo getThisPredecessor();
-    public Node.NodeInfo getThisSuccessor();
-    public Node.NodeInfo successor(BigInteger id);
+    public String lookup(String word) throws RemoteException;
+    public Node.NodeInfo predecessor(BigInteger id) throws RemoteException;
+    public Node.NodeInfo getThisPredecessor() throws RemoteException;
+    public Node.NodeInfo getThisSuccessor() throws RemoteException;
+    public Node.NodeInfo successor(BigInteger id) throws RemoteException;
     
-    public void insertKey(String word, String meaning);
-    public void removeKey(String word);
-    public void updateSuccessor(Node.NodeInfo successor);
-    public void notify(Node.NodeInfo predecessor);
+    public void insertKey(String word, String meaning) throws RemoteException;
+    public void removeKey(String word) throws RemoteException;
+    public void updateSuccessor(Node.NodeInfo successor) throws RemoteException;
+    public void notify(Node.NodeInfo predecessor) throws RemoteException;
     
-    public void fixFingers();
-    public Hashtable<String, String> getKeyStore();
-    public void printRingStructure();
-	public String getFormattedNodeDetails();
+    public void fixFingers() throws RemoteException;
+    public Hashtable<String, String> getKeyStore()  throws RemoteException;
+    public void printRingStructure() throws RemoteException;
+	public String getFormattedNodeDetails() throws RemoteException;
     
 }
